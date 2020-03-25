6a1a4465e94a69db6a1c4eb4f8cdfd5282a72471
kotaok <kota813e@gmail.com>
2020-03-25 18:05:57 +0900

    作品詳細のダイアログ機能実装

--- src/components/works.vue
+++ src/components/works.vue
@@ -2,9 +2,26 @@
   <div id="demo">
     <transition-group tag="div" class="list" appear>
       <div class="item" stagger="70" v-for="item of items" v-bind:key="item.name">
-        <img :src="item.path" :alt="item.name" width="100%" height="200px">
+        <img 
+          :src="item.path" 
+          :alt="item.name" 
+          @click="
+          dialogVisible = true,
+          dialogname = item.name,
+          dialogtext = item.text"
+          width="100%" height="200px"
+        >
       </div>
     </transition-group>
+    <div>
+    <el-dialog
+      :title="dialogname"
+      :visible.sync="dialogVisible"
+      width="30%"
+      :before-close="handleClose">
+      <span>{{ dialogtext }}</span>
+    </el-dialog>
+  </div>
   </div>
 </template>
 
@@ -13,12 +30,21 @@ export default {
   data: function() {
     return {
       items: [
-        { name: 'slackbot',path: '/img/slackbot.png'},
-        { name: 'portfoliov1',path: '/img/kotaportfolio.png'},
-        { name: 'webrain',path: '/img/webrain.png'},
-        { name: 'kashiwa',path: '/img/kashiwa.png'}
+        { name: 'slackbot',path: '/img/slackbot.png',text:'PythonのSlackBotライブラリを使って開発した翻訳Bot'},
+        { name: 'portfolio_ver1',path: '/img/kotaportfolio.png',text: 'HTMLとCSSの練習を兼ねて製作した最初のポートフォリオ'},
+        { name: 'webrain',path: '/img/webrain.png',text: 'Node.js/Express,Firebaseを使って開発した「ブレインストーミング」のアイデアを出し合えるチャットアプリ'},
+        { name: 'kashiwa',path: '/img/kashiwa.png',text: '通学しているN高柏キャンパスのホームページ開発'},
+        { name: 'portfolio_ver2',path: '/img/portfolio_2.png',text: 'Vue.jsを使って開発した新しいポートフォリオ'}
       ],
+      dialogVisible: false,
+      dialogname: "test",
+      dialogtext: "test",
     };
+  },
+  methods: {
+    handleClose(done) {
+      done();
+    }
   }
 };
 </script>

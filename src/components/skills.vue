b4c4bf8cda44d95b8821393a12a0bab6424f7ed8
kotaok <kota813e@gmail.com>
2020-03-26 23:49:18 +0900

    グラフの数値に文字列を指定していたミスを修正

--- src/components/skills.vue
+++ src/components/skills.vue
@@ -14,13 +14,13 @@ export default {
   data: function() {
     return {
       skills: [
-        { name: 'HTML・CSS', percent: '60'},
-        { name: 'JavaScript', percent: '30'},
-        { name: 'C++', percent: '30'},
-        { name: 'Python', percent: '20'},
-        { name: 'Node.js/Express', percent: '40'},
-        { name: 'Vue.js', percent: '20'},
-        { name: 'Firebase', percent: '50'}
+        { name: 'HTML・CSS', percent: 60},
+        { name: 'JavaScript', percent: 30},
+        { name: 'C++', percent: 30},
+        { name: 'Python', percent: 20},
+        { name: 'Node.js/Express', percent: 40},
+        { name: 'Vue.js', percent: 20},
+        { name: 'Firebase', percent: 50}
       ],
     };
   }

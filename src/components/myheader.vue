57a42b45c79049a4808b76662e27a2934fde80c2
kotaok <kota813e@gmail.com>
2020-04-16 19:44:05 +0900

    ヘッダーのデザイン改善

--- src/components/myheader.vue
+++ src/components/myheader.vue
@@ -14,10 +14,21 @@
   display: flex;
   justify-content: center;
   padding: 15%;
-  background-image: url("/img/keyimage.png");
-  background-size: cover;
+  background: #005AA7;  /* fallback for old browsers */
+  background: -webkit-linear-gradient(to right, #FFFDE4, #005AA7);  /* Chrome 10-25, Safari 5.1-6 */
+  background: linear-gradient(to right, #FFFDE4, #005AA7); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+}
+
+.header div {
+  position: relative;
+  left: 0;
 }
 
 .header div p {
+  font-family: bilo, sans-serif;
+  font-weight: 700;
+  font-style: normal;
+  font-size: 50px;
+  color: #dedede;
 }
 </style>
